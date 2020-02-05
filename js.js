7668eb6ad5dbcd6d76ee75d9f501c701db8c0420
thientu995 <ttvn95@gmail.com>
2020-02-05 11:44:08 +0700

    Add files via upload

--- js.js
+++ js.js
@@ -4,26 +4,62 @@ $(document).ready(function () {
         async: true,
         success: function (res) {
             res.forEach(group => {
-                let objGroup = $('#' + group.Category);
-                let contentHtml = objGroup.html();
-                let valueAttr = objGroup.attr('data-attr').split(',');
-                objGroup.html('');
-                group.Category
-                group.Data.forEach(data => {
-                    let htmlObj = contentHtml;
-                    valueAttr.forEach(attr => {
-                        let re = new RegExp('{{' + attr + '}}', 'g');
-                        if (Array.isArray(data[attr])) {
-                            data[attr] = data[attr].join('');
-                        }
-                        htmlObj = htmlObj.replace(re, data[attr]);
-                    });
-                    objGroup.append(htmlObj);
-                });
+                SetValueHeader(group);
+                SetValueContent(group);
+                // let objGroup = $('#' + group.Category + ' *[data-content!=""]');
+                // let contentHtml = objGroup.html();
+                // console.log(objGroup)
+                // let valueAttr = objGroup.attr('data-content').split(',');
+                // objGroup.html('');
+                // group.Data.forEach(data => {
+                //     let htmlObj = contentHtml;
+                //     valueAttr.forEach(attr => {
+                //         let re = new RegExp('{{' + attr + '}}', 'g');
+                //         if (Array.isArray(data[attr])) {
+                //             data[attr] = data[attr].join('');
+                //         }
+                //         htmlObj = htmlObj.replace(re, data[attr]);
+                //     });
+                //     objGroup.append(htmlObj);
+                // });
             });
             $('body').fadeIn(1500);
         }
     });
+
+    //Function
+    function SetValueContent(group) {
+        let objGroup = $('#' + group.Category + ' *[data-content!=""]');
+        objGroup = objGroup.filter('[data-content]');
+        
+        if (objGroup.attr('data-content') == null) return;
+
+        let contentHtml = objGroup.html();
+        let valueAttr = objGroup.attr('data-content').split(',');
+        objGroup.html('');
+        group.Data.forEach(data => {
+            let htmlObj = contentHtml;
+            valueAttr.forEach(attr => {
+                let re = new RegExp('{{' + attr + '}}', 'g');
+                if (Array.isArray(data[attr])) {
+                    data[attr] = data[attr].join('');
+                }
+                htmlObj = htmlObj.replace(re, data[attr]);
+            });
+            objGroup.append(htmlObj);
+        });
+    }
+
+
+    function SetValueHeader(group) {
+        let objGroup = $('#' + group.Category + ' *[data-attr="Header"]');
+        if (objGroup.length == 0) return;
+        let contentHtml = objGroup.html();
+        contentHtml = contentHtml.replace(new RegExp('{{Icon}}', 'g'), group.Icon);
+        contentHtml = contentHtml.replace(new RegExp('{{Text}}', 'g'), group.Text);
+        objGroup.html('');
+        objGroup.append(contentHtml);
+    }
 });
 
 function Print(size) {
