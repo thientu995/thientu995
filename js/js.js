36dcf3f4d9462ff17f39d8cff327085b6f1dd5fe
thientu995 <thientu995@gmail.com>
2020-02-05 21:33:56 +0700

    edit
    Signed-off-by: thientu995 <thientu995@gmail.com>

---
+++ js/js.js
@@ -0,0 +1,76 @@
+class Init {
+    constructor() {
+        this.isPrint = false;
+        const self = this;
+        const params = new URL(location.href).searchParams;
+        if (params.get('print') != null) {
+            const size = params.get('print');
+            self.Print(size);
+        }
+        $.ajax({
+            url: 'data.json',
+            async: true,
+            success: function(res) {
+                res.forEach(group => {
+                    self.SetValueHeader(group);
+                    self.SetValueContent(group);
+                });
+                self.ProcessLoadPage();
+            }
+        });
+    }
+
+    ProcessLoadPage() {
+        if (!this.isPrint) {
+            setTimeout(function() {
+                new SimpleBar(document.getElementById('body'), { autoHide: true, height: 'auto' })
+            });
+        }
+
+        $('.loader').hide();
+        $('.content').fadeIn(1500);
+    }
+
+    SetValueContent(group) {
+        let objGroup = $('#' + group.Category + ' *[data-content!=""]');
+        objGroup = objGroup.filter('[data-content]');
+
+        if (objGroup.attr('data-content') == null) return;
+
+        let contentHtml = objGroup.html();
+        let valueAttr = objGroup.attr('data-content').split(',');
+        objGroup.html('');
+        group.Data.forEach(data => {
+            let htmlObj = contentHtml;
+            valueAttr.forEach(attr => {
+                let re = new RegExp('{{' + attr + '}}', 'g');
+                if (Array.isArray(data[attr])) {
+                    data[attr] = data[attr].join('');
+                }
+                htmlObj = htmlObj.replace(re, data[attr]);
+            });
+            objGroup.append(htmlObj);
+        });
+    }
+
+    SetValueHeader(group) {
+        let objGroup = $('#' + group.Category + ' *[data-attr="Header"]');
+        if (objGroup.length == 0) return;
+        let contentHtml = objGroup.html();
+        contentHtml = contentHtml.replace(new RegExp('{{Icon}}', 'g'), group.Icon);
+        contentHtml = contentHtml.replace(new RegExp('{{Text}}', 'g'), group.Text);
+        objGroup.html('');
+        objGroup.append(contentHtml);
+    }
+
+    Print(size) {
+        this.isPrint = true;
+        size = size || 210;
+        $('footer,header').hide();
+        $('html').css('width', size + 'mm');
+    }
+}
+
+$(document).ready(function() {
+    let init = new Init();
+});
