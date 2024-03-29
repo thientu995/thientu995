ece1675b6b58a704613fc17024040ffdba042d2e
Tu Nguyen Thien (SM) <tu.nguyen.thien@stepmediasoftware.com>
2024-03-29 21:35:56 +0700

    deploy v3

--- README.md
+++
@@ -1,8 +0,0 @@
-# 👋  Hi, I'm Thien Tu from :vietnam:
-
-<p align="center">
-  <img src="https://github-readme-stats.vercel.app/api?username=thientu995&show_icons=true&include_all_commits=true&count_private=true&card_width=445" alt="Nguyen Thien Tu" />
-</p>
-<p align="center">
-  <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=thientu995&langs_count=10&layout=compact&count_private=true&card_width=445" alt="Nguyen Thien Tu" />
-</p>
--- functions/index.js
+++ functions/index.js
@@ -1,16 +1,14 @@
 const redirectHttpCode = 301
-const redirectMap = 'https://t4vn.com'
-const hostnameExclude = 'pages.dev'
+const hostnameRedirect = 'tunt.net'
+const redirectMap = `https://${hostnameRedirect}`
 const lengthHostName = hostnameExclude.split('.').length + 1
 
 export async function onRequest(context) {
     const url = new URL(context.request.url)
     const { hostname } = url
 
-    if (hostname.substring(hostname.lastIndexOf(hostnameExclude)) === hostnameExclude) {
-        if (hostname.split('.').length === lengthHostName) {
-            return Response.redirect(redirectMap, redirectHttpCode)
-        }
+    if (hostname != hostnameRedirect) {
+        return Response.redirect(redirectMap, redirectHttpCode)
     }
     return await context.next();
 }
